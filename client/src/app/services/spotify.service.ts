93249191dc8ecaa50d6298d3816829b1c6af3546
Daniel Epstein <epstein@ics.uci.edu>
2018-10-27 08:53:01 -0700

    Initial import

---
+++ client/src/app/services/spotify.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+	expressBaseUrl:string = 'http://localhost:8888';
+
+  constructor(private http:HttpClient) { }
+
+  private sendRequestToExpress(endpoint:string):Promise<any> {
+    //TODO: use the injected http Service to make a get request to the Express endpoint and return the response.
+    //the http service works similarly to fetch(). It may be useful to call .toPromise() on any responses.
+    //update the return to instead return a Promise with the data from the Express server
+    return Promise.resolve();
+  }
+
+  //TODO: create functions for all of the endpoints on the webserver (except for /login, which is linked to from a button in the navbar).
+}
